78db702b5ca5b20041ed44c8b8b57920b647ffda
barriquello <barriquello@gmail.com>
2016-02-18 13:37:41 -0200

    add mk files

---
+++ boards/RoteadorCFV1_Eclipse/Debug/Sources/BRTOS/brtos/subdir.mk
@@ -0,0 +1,94 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../../../brtos/brtos/BRTOS.c \
+../../../brtos/brtos/OSInfo.c \
+../../../brtos/brtos/OSTime.c \
+../../../brtos/brtos/mbox.c \
+../../../brtos/brtos/mutex.c \
+../../../brtos/brtos/queue.c \
+../../../brtos/brtos/semaphore.c \
+../../../brtos/brtos/stimer.c 
+
+OBJS += \
+./Sources/BRTOS/brtos/BRTOS.o \
+./Sources/BRTOS/brtos/OSInfo.o \
+./Sources/BRTOS/brtos/OSTime.o \
+./Sources/BRTOS/brtos/mbox.o \
+./Sources/BRTOS/brtos/mutex.o \
+./Sources/BRTOS/brtos/queue.o \
+./Sources/BRTOS/brtos/semaphore.o \
+./Sources/BRTOS/brtos/stimer.o 
+
+C_DEPS += \
+./Sources/BRTOS/brtos/BRTOS.d \
+./Sources/BRTOS/brtos/OSInfo.d \
+./Sources/BRTOS/brtos/OSTime.d \
+./Sources/BRTOS/brtos/mbox.d \
+./Sources/BRTOS/brtos/mutex.d \
+./Sources/BRTOS/brtos/queue.d \
+./Sources/BRTOS/brtos/semaphore.d \
+./Sources/BRTOS/brtos/stimer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Sources/BRTOS/brtos/BRTOS.o: ../../../brtos/brtos/BRTOS.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Coldfire C Compiler'
+	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I../Startup_Code -I../Project_Headers -I../Project_Headers/CONFIG -I../../../brtos/hal/MemoryAllocation -I../../../brtos/brtos/includes -I../../../brtos/hal/GCC_CFV1 -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/BRTOS/brtos/OSInfo.o: ../../../brtos/brtos/OSInfo.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Coldfire C Compiler'
+	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I../Startup_Code -I../Project_Headers -I../Project_Headers/CONFIG -I../../../brtos/hal/MemoryAllocation -I../../../brtos/brtos/includes -I../../../brtos/hal/GCC_CFV1 -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/BRTOS/brtos/OSTime.o: ../../../brtos/brtos/OSTime.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Coldfire C Compiler'
+	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I../Startup_Code -I../Project_Headers -I../Project_Headers/CONFIG -I../../../brtos/hal/MemoryAllocation -I../../../brtos/brtos/includes -I../../../brtos/hal/GCC_CFV1 -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/BRTOS/brtos/mbox.o: ../../../brtos/brtos/mbox.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Coldfire C Compiler'
+	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I../Startup_Code -I../Project_Headers -I../Project_Headers/CONFIG -I../../../brtos/hal/MemoryAllocation -I../../../brtos/brtos/includes -I../../../brtos/hal/GCC_CFV1 -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/BRTOS/brtos/mutex.o: ../../../brtos/brtos/mutex.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Coldfire C Compiler'
+	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I../Startup_Code -I../Project_Headers -I../Project_Headers/CONFIG -I../../../brtos/hal/MemoryAllocation -I../../../brtos/brtos/includes -I../../../brtos/hal/GCC_CFV1 -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/BRTOS/brtos/queue.o: ../../../brtos/brtos/queue.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Coldfire C Compiler'
+	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I../Startup_Code -I../Project_Headers -I../Project_Headers/CONFIG -I../../../brtos/hal/MemoryAllocation -I../../../brtos/brtos/includes -I../../../brtos/hal/GCC_CFV1 -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/BRTOS/brtos/semaphore.o: ../../../brtos/brtos/semaphore.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Coldfire C Compiler'
+	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I../Startup_Code -I../Project_Headers -I../Project_Headers/CONFIG -I../../../brtos/hal/MemoryAllocation -I../../../brtos/brtos/includes -I../../../brtos/hal/GCC_CFV1 -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/BRTOS/brtos/stimer.o: ../../../brtos/brtos/stimer.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Coldfire C Compiler'
+	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I../Startup_Code -I../Project_Headers -I../Project_Headers/CONFIG -I../../../brtos/hal/MemoryAllocation -I../../../brtos/brtos/includes -I../../../brtos/hal/GCC_CFV1 -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ boards/RoteadorCFV1_Eclipse/Debug/Sources/BRTOS/hal/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../../../brtos/hal/GCC_CFV1/HAL.c 
+
+OBJS += \
+./Sources/BRTOS/hal/HAL.o 
+
+C_DEPS += \
+./Sources/BRTOS/hal/HAL.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Sources/BRTOS/hal/HAL.o: ../../../brtos/hal/GCC_CFV1/HAL.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Coldfire C Compiler'
+	m68k-elf-gcc -mcpu=51 -g3 -Os -ffunction-sections -fdata-sections -DDEBUG_BUILD -I../Startup_Code -I../Project_Headers -I../Project_Headers/CONFIG -I../../../brtos/hal/MemoryAllocation -I../../../brtos/brtos/includes -I../../../brtos/hal/GCC_CFV1 -Wall -std=c99 -c -fmessage-length=0 -MT"$@" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -o "$@" $<
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
